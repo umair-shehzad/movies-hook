99252fb297cf332c37dc62f239b5d1828b88df60
Umair Shehzad <umairshehzad247365@gmail.com>
2023-03-27 10:31:43 +0500

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Movies } from "./movies/Movies.jsx";
+export { default as Movie } from "./Movies/Movie/Movie";
+export { default as Search } from "./Search/Search.jsx";
+export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as MovieInfo } from "./Movies/MovieInformation/MovieInformation";
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
+import './index.css';
+import App from './components/App';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
+// commnts
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
